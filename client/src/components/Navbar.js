0c464638ca38213f28bb849059ea4fca5f621d7f
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-05-17 02:12:54 -0400

    simplify navbar with flex

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,62 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link }from 'react-router-dom';
 
-const height = 60;
-const heightString = height.toString() + 'px';
-
 const Navigation =  styled.div`
-  background: #000000;
-  position: fixed;
-  top: 0;
-  font-family: 'Elephant';
-  display: flexbox;
-  height: ${heightString};
   width: 100%;
-  > * {
-    display: flex;
-  }
+  color: #fff;
+  background: #000;
+  display: flex;
+  align-items: center;
+  height: 60px;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  font-family: 'Elephant';
+  overflow: hidden;
 `
 const IconContainer = styled.div`
   border-right: 1px solid #343434;
-  position: relative;
-  width:130px;
+  text-align: center;
+  width: 130px;
 `
 const Icon = styled.h2`
-  /* font-size: 14px; */
-  position: absolute;
-  text-align: center;
   color: white;
-  height: 100;
-  width:100%;
-  margin: 0;
-  text-transform: uppercase;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
+  height:  100%;
 `
 const Selections = styled.div`
-  position: relative;
-  padding-left: 15px;
-  padding-right: 15px;
+  flex: 1;
+  display: flex;
+  padding-left:15px;
 `
 const NavLink = styled.p`
-  position: relative;
-  font-size: 13px;
   color: white;
-  text-transform: uppercase;
-  text-align: center;
-  width: 100%;
-  margin: 0;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-`;
-
-const LinkContainer = styled.div`
-  height: ${heightString};
-  margin-left: 15px;
-  margin-right: 15px;
-  /* background: pink; */
+  font-size: 14px;
+  &:hover {
+   color: gray;
+  }
 `;
 
 class Navbar extends React.Component {
@@ -65,10 +42,13 @@ class Navbar extends React.Component {
   render() {
     const options =  ['brands', 'bategories', 'realeases'].map((link) => {
       return(
-        <Link to={`${link}`} style={{textDecoration: 'none'}}>
-          <LinkContainer>
-            <NavLink>{`${link}`}</NavLink>
-          </LinkContainer>
+        <Link
+          to={`${link}`}
+          style={{
+            textDecoration:'none',
+            marginLeft: '15px',
+            marginRight: '15px'}}>
+          <NavLink>{`${link}`}</NavLink>
         </Link>
       );
     })
@@ -76,7 +56,7 @@ class Navbar extends React.Component {
     return (
       <Navigation>
         <IconContainer>
-          <Link to="/">
+          <Link to="/" style={{textDecoration: 'none'}}>
             <Icon>
               Rebel
             </Icon>
